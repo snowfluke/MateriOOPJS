b570d5c1c2f998c923816bb505383d46ddb96d4b
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-04-09 18:54:22 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// Program menampilkan nama
+
+// Deklarasi variable fadhli menggunakan let
+let fadhli = 'Nurfadhli Abdurrachman Hakim'
+let henky = 'Henky Fajar Syafani'
+
+// sebuah function untuk menampilkan nama
+// berdasarkan argumen yang dikirimkan
+// function ini untuk sementara diabaikan terlebih dahulu
+function tampilkanNama(nama){
+    console.log(nama)
+}
+
+// mengeksekusi function tampilkanNama dengan argumen
+// berupa variabel fadhli dan henky
+tampilkanNama(fadhli)
+tampilkanNama(henky)
