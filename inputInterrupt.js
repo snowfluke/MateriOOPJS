d12529a502268328ba42d26cbe9233bdd0a44218
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-04-20 22:34:53 +0700

    Create inputInterrupt.js

---
+++ inputInterrupt.js
@@ -0,0 +1,9 @@
+// Pastikan kalian telah menginstall readline-sync
+// npm install readline-sync
+// Membaca inputan dan memasukkanya ke variable nama
+let baca = require('readline-sync')
+let nama = baca.question("Siapa nama kamu? ")
+
+console.log(`Nama kamu adalah ${nama}!`)
+
+// Cobalah bereksperimen
