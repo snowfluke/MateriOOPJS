bec172ca9f867d841ab7c64e51cec82dc10909c6
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-04-20 22:37:01 +0700

    Create perulanganFor.js

---
+++ perulanganFor.js
@@ -0,0 +1,14 @@
+
+class Perulangan{
+    main(){
+        for(let a = 1; a <=10; a++){
+            console.log(`Saya berjanji akan mengerjakan semua challenge`)
+        }
+    }
+}
+
+const coba = new Perulangan()
+
+coba.main()
+// Akan tampil 10 baris
+
